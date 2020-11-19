094f4fc8a950eaa0c32f94501787313e9709d612
Justin <32024194+JustinZ04@users.noreply.github.com>
2020-11-19 16:59:11 -0500

    Update substation_info.py

--- config/substation_info.py
+++ config/substation_info.py
@@ -1,8 +1,8 @@
 # info on substation stanton
-id = "28"
-panel_area = 25172
-panel_eff = .18
-kw = 1000
+id = "28" # substation id
+panel_area = 25172 # solar panel area
+panel_eff = .18 # solar panel efficiency
+kw = 1000 # conversion factor
 
 LAT = 28.601722
 LONG = -81.198545
