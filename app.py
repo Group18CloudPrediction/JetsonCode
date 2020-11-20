51b7d50756486dbb8093d50f99bc2fe58785dd0c
Gustaf Jiborn <gjiborn@gmail.com>
2020-11-20 01:39:01 -0500

    Removed all unnecessary cloud_tracking files
    +Commented app.py and reformatted

--- app.py
+++ app.py
@@ -1,54 +1,40 @@
-import subprocess as sp
 import base64
-import time
+import subprocess as sp
 import sys
+import time
+
 import cv2
 import numpy as np
+import pymongo
 import socketio
-from multiprocessing import Process, Queue
 
-import forecast
+from config import cloud_tracking_config as ct_cfg, creds, substation_info as substation_cfg
+from imageProcessing import fisheye_mask as fisheye, coverage
 from opticalFlow import opticalDense
-from coverage import coverage
-from fisheye_mask import create_mask
-from sunPos import mask_sun
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+
+def current_milli_time(): return int(round(time.time() * 1000))
+
 
 # Constants
 # URL_APP_SERVER          = 'http://localhost:3001/'
-URL_APP_SERVER          = 'https://cloudtracking-v2.herokuapp.com/'
-DISPLAY_SIZE            = (512, 384)
-MASK_RADIUS_RATIO       = 3.5
-SECONDS_PER_FRAME       = 1
-SECONDS_PER_PREDICTION  = 30
+URL_APP_SERVER = 'https://cloudtracking-v2.herokuapp.com/'
+DISPLAY_SIZE = (512, 384)
+MASK_RADIUS_RATIO = 3.5
+SECONDS_PER_FRAME = 1
 
-#
-# LAT  = 28.603865
-# LONG = -81.199273
-
-# Lake Claire
-# LAT = 28.607334
-# LONG = -81.203706
-
-# Garage C
-# LAT = 28.601985
-# LONG = -81.195806
-
-# Engineering II
-LAT = 28.601722
-LONG = -81.198545
+sock = None
+db = None
 
 # FLAGS -- used to test different functionalities
-display_images = True
 send_images = True
 do_coverage = True
 do_mask = True
 do_crop = True
-sock = None
 
-# Initialize socket io
+
 def initialize_socketio(url):
+    """Initialize socket io"""
     sio = socketio.Client()
 
     @sio.event
@@ -58,17 +44,9 @@ def initialize_socketio(url):
     sio.connect(url)
     return sio
 
-def send_predictions(data):
-    if sock is None:
-        return
-
-    payload = {
-        'cloudPrediction': {int(a) : int(b) for a,b in data}
-    }
-
-    sock.emit('predi', payload)
 
 def send_coverage(coverage):
+    """Sends percent cloud coverage to database"""
     if sock is None:
         return
 
@@ -77,11 +55,19 @@ def send_coverage(coverage):
 
     coverage = np.round((cloud / (cloud + not_cloud)) * 100, 2)
 
-    print(coverage)
+    post = {
+        "author": "cloud_tracking.py",
+        "cloud_coverage": coverage,
+        "system_num": substation_cfg.id
+    }
 
-    sock.emit('coverage_data', { "cloud_coverage": coverage })
+    posts = db.cloudCoverageData
+    post_id = posts.insert_one(post).inserted_id
+    print("DB sent -> cover_post_id: " + str(post_id))
+
 
 def send_image(image, event_name):
+    """Emits an image through socketIO connection"""
     if send_images is False or sock is None:
         return
     success, im_buffer = cv2.imencode('.png', image)
@@ -91,67 +77,50 @@ def send_image(image, event_name):
         return
 
     byte_image = im_buffer.tobytes()
-    sock.emit(event_name + '27', byte_image)
+    sock.emit(event_name + substation_cfg.id, byte_image)
 
-# send coverage image
+
 def send_cloud(frame):
+    """Sends cloud image to website via socketIO"""
     send_image(frame, 'coverage')
 
+
 def send_shadow(coverage):
+    """Sends shadow image to website via socketIO"""
     shadow = coverage.copy()
     shadow[(shadow[:, :, 3] > 0)] = (0, 0, 0, 127)
     send_image(shadow, 'shadow')
 
-def forecast_(queue, prev, next):
-    before_ = current_milli_time()
-    sun_center, sun_pixels = mask_sun(LAT, LONG)
-    after_mask = current_milli_time()
-    times = forecast.forecast(sun_pixels, prev, next, 1/SECONDS_PER_FRAME)
-    after_forecast = current_milli_time()
 
-    prediction_frequencies = np.array(np.unique(np.round(times), return_counts=True)).T
-
-    queue.put(prediction_frequencies)
-
-    elapsed_mask = (after_mask - before_)
-    print('SUN MASK TOOK: %s ms' % elapsed_mask)
-
-    elapsed_forecast = (after_forecast - after_mask)
-    print('FORECAST TOOK: %s ms' % elapsed_forecast)
-
-# Make all black pixels transparent
 def black2transparent(bgr):
+    """Make all black pixels transparent"""
     bgra = cv2.cvtColor(bgr, cv2.COLOR_BGR2BGRA)
     bgra[(bgra[:, :, 0:3] == [0, 0, 0]).all(2)] = (0, 0, 0, 0)
     return bgra
 
+
 def experiment_step(prev, next):
+    """Perform image processing + optical flow on the two inputted frames"""
     before = current_milli_time()
     clouds = None
+
+    # Apply fisheye mask
     if do_mask is True:
-        mask = create_mask.create_mask(prev, MASK_RADIUS_RATIO)
-        prev = create_mask.apply_mask(prev, mask)
-        next = create_mask.apply_mask(next, mask)
+        prev, next = fisheye.create_fisheye_mask(prev, next)
 
+    # crop image to square to eliminate curved lens interference
     if do_crop is True:
-        w = prev.shape[0]
-        h = prev.shape[1]
-        s = w / MASK_RADIUS_RATIO
-
-        top_edge = int(h/2-s)
-        bottom_edge = int(h/2 + s)
-
-        left_edge = int(w/2-s)
-        right_edge = int (w/2 + s)
-        prev = prev[ left_edge:right_edge  ,  top_edge:bottom_edge , :]
-        next = next[ left_edge:right_edge  ,  top_edge:bottom_edge , :]
+        prev = fisheye.image_crop(prev)
+        next = fisheye.image_crop(next)
 
     # Find the flow vectors for the prev and next images
     flow_vectors = opticalDense.calculate_opt_dense(prev, next)
 
+    # Convert pixel opacity values to not cloud (0) or cloud (1) -> based on pixel saturation
     if do_coverage is True:
         clouds = coverage.cloud_recognition(next)
 
+    # Draw vector field based on cloud/not cloud image and displacement vectors
     flow, _, __ = opticalDense.draw_arrows(clouds.copy(), flow_vectors)
 
     after = current_milli_time()
@@ -161,66 +130,47 @@ def experiment_step(prev, next):
     # Return experiment step
     return (prev, next, flow, clouds)
 
-def experiment_display(prev, next, flow, coverage):
-    if display_images is False:
-        return
-    # Resize the images for visibility
-    flow_show = cv2.resize(flow, DISPLAY_SIZE)
-    prev_show = cv2.resize(prev, DISPLAY_SIZE)
-    next_show = cv2.resize(next, DISPLAY_SIZE)
 
-    # Show the images
-    cv2.imshow('flow?', flow_show)
-    cv2.imshow('previous', prev_show)
-    cv2.imshow('next', next_show)
-
-    # Wait 30s for ESC and return false if pressed
-    k = cv2.waitKey(30) & 0xff
-    if (k == 27):
-        return False
-    return True
-
-def create_ffmpeg_pipe(video_path = None):
-    if video_path is None:
-        command = [ 'ffmpeg',
-            '-loglevel', 'panic',
-            '-nostats',
-            '-rtsp_transport', 'tcp',
-            '-i', 'rtsp://192.168.0.10:8554/CH001.sdp',
-            '-s', '1024x768',
-            '-f', 'image2pipe',
-            '-pix_fmt', 'rgb24',
-            '-vf', 'fps=fps=1/8',
-            '-vcodec', 'rawvideo', '-']
+def create_ffmpeg_pipe():
+    """Creates + executes command ffmpeg for video stream, and returns pipe"""
+    if ct_cfg.livestream_online is True:
+        command = ['ffmpeg',
+                   '-loglevel', 'panic',
+                   '-nostats',
+                   '-rtsp_transport', 'tcp',
+                   '-i', 'rtsp://192.168.0.10:8554/CH001.sdp',
+                   '-s', '1024x768',
+                   '-f', 'image2pipe',
+                   '-pix_fmt', 'rgb24',
+                   '-vf', 'fps=fps=1/8',
+                   '-vcodec', 'rawvideo', '-']
     else:
-        command = [ 'ffmpeg',
-            '-loglevel', 'panic',
-            '-nostats',
-            '-i', video_path,
-            '-s', '1024x768',
-            '-f', 'image2pipe',
-            '-pix_fmt', 'rgb24',
-            '-vcodec', 'rawvideo', '-']
+        command = ['ffmpeg',
+                   '-loglevel', 'panic',
+                   '-nostats',
+                   '-i', ct_cfg.VIDEO_PATH,
+                   '-s', '1024x768',
+                   '-f', 'image2pipe',
+                   '-pix_fmt', 'rgb24',
+                   '-vcodec', 'rawvideo', '-']
 
-    pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
+    pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10**8)
     return pipe
+
 
 def experiment_ffmpeg_pipe(pipe):
     before = current_milli_time()
 
-    ## BRONZE SOLUTION
+    # BRONZE SOLUTION
     First = True
     BLOCK = False
-
-
-    prediction_queue = Queue()
 
     while True:
         try:
             prev_rawimg = pipe.stdout.read(1024*768*3)
             # transform the byte read into a numpy array
-            prev =  np.fromstring(prev_rawimg, dtype='uint8')
-            prev = prev.reshape((768,1024,3))
+            prev = np.fromstring(prev_rawimg, dtype='uint8')
+            prev = prev.reshape((768, 1024, 3))
             prev = cv2.cvtColor(prev, cv2.COLOR_RGB2BGR)
             prev = np.fliplr(prev)
 
@@ -229,8 +179,8 @@ def experiment_ffmpeg_pipe(pipe):
 
             next_rawimg = pipe.stdout.read(1024*768*3)
             # transform the byte read into a np array
-            next =  np.fromstring(next_rawimg, dtype='uint8')
-            next = next.reshape((768,1024,3))
+            next = np.fromstring(next_rawimg, dtype='uint8')
+            next = next.reshape((768, 1024, 3))
             next = cv2.cvtColor(next, cv2.COLOR_RGB2BGR)
             next = np.fliplr(next)
 
@@ -240,27 +190,10 @@ def experiment_ffmpeg_pipe(pipe):
             (prev, next, flow, coverage) = experiment_step(prev, next)
 
             after = current_milli_time()
-            if (after - before > (1000 * SECONDS_PER_PREDICTION)
-                or First is True) and BLOCK is False:
-                BLOCK = True
-                p = Process(target=forecast_, args=(prediction_queue, prev, next))
-                p.start()
-                First = False
-                before = after
-
-            if(prediction_queue.empty() != True):
-                prediction_frequencies = prediction_queue.get()
-                print("Sending predictions", np.shape(prediction_frequencies))
-                send_predictions(prediction_frequencies)
-                BLOCK = False
 
             send_cloud(flow)
             send_shadow(coverage)
-            # send_coverage(coverage)
-
-            # Break if ESC key was pressed
-            if (experiment_display(prev, next, flow, coverage) == False):
-                break
+            send_coverage(coverage)
         except Exception as inst:
             print(inst)
             break
@@ -270,10 +203,17 @@ def experiment_ffmpeg_pipe(pipe):
 def main():
     global sock
     sock = initialize_socketio(URL_APP_SERVER)
-    pipe = create_ffmpeg_pipe(None)
+
+    global db
+    client = pymongo.MongoClient("mongodb+srv://" + creds.username + ":" +
+                                 creds.password + "@cluster0.lgezy.mongodb.net/<dbname>?retryWrites=true&w=majority")
+    db = client.cloudTrackingData
+
+    pipe = create_ffmpeg_pipe()
 
     experiment_ffmpeg_pipe(pipe)
     if sock is not None:
         sock.disconnect()
+
 
 main()
--- imageProcessing/coverage.py
+++ imageProcessing/coverage.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from scipy import signal
 
 
 def _calc_sat(r, g, b):
@@ -21,13 +22,37 @@ def _calc_sat(r, g, b):
 v_sat = np.vectorize(_calc_sat)
 
 
-def cloud_recognition(img, SAT_THRESHOLD=0.08):
+def cloud_recognition(img, SAT_THRESHOLD=0.08, SUN_RADIUS=50, SUN_THRESHOLD=2.9375, FILTER_SIZE=25):
     """Converts all pixel RGBs in an image to 0 (not cloud) or 255 (cloud)"""
     # OpenCV opens images as GBR. We need to change it to RGB, convert it to a numpy array
     # and then normalize all the values
     img = np.asarray(img).astype(np.double)
     img /= 255
 
+    # Locate the brightest pixel in the image (i.e. the sun) and cover it up so it doesn't
+    # f*ck with our coverage code
+    intensity = img[:, :, 0] + img[:, :, 1] + img[:, :, 2]
+
+    # To eliminate noise and find the center of the sun, we're going to do a mean convolution
+    mean_matrix = np.full(shape=(FILTER_SIZE, FILTER_SIZE),
+                          fill_value=1/FILTER_SIZE**2)
+    convolved_intensity = signal.convolve2d(
+        intensity, mean_matrix, mode='full', boundary='fill', fillvalue=0)
+
+    # locate the brightest pixel in the image (aka the pixel with the highest intensity value)
+    max_intensity = np.amax(convolved_intensity)
+
+    # If the sun is in the image, the max_intensity should be greater than this threshold
+    # If it's not, the that means that the sun is probably covered by a cloud (or not in frame)
+    if max_intensity >= SUN_THRESHOLD:
+        brightest = np.where(convolved_intensity == max_intensity)
+        l = int(len(brightest[0]) / 2)
+
+        x = brightest[1][l]
+        y = brightest[0][l]
+        r = SUN_RADIUS
+
+        cv2.circle(img, (x, y), r, (255, 0, 0), -1)
     # Use the vectorized functions above and apply to every element of the matrix
     sat = v_sat(img[:, :, 2], img[:, :, 1], img[:, :, 0])
 
